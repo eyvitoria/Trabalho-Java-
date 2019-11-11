d1bd0e10b561d9ddbcc853376f92c2cd57f565f7
Emilly Vitoria <vitoriamatias152@gmail.com>
2019-11-11 16:10:02 -0300

    Tela inicial

--- src/trabalhojava/TrabalhoJava.java
+++ src/trabalhojava/TrabalhoJava.java
@@ -16,6 +16,7 @@ public class TrabalhoJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new inicio().setVisible(true);
     }
     
 }
