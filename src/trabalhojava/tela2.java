5554dc8c1938013f7d522702da79797006263671
Emilly Vitoria <vitoriamatias152@gmail.com>
2019-11-17 11:13:56 -0300

    Teste

--- src/trabalhojava/tela2.java
+++ src/trabalhojava/tela2.java
@@ -50,7 +50,7 @@ public class tela2 extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
     new batalha().setVisible(true);
-    dispose();
+   dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
